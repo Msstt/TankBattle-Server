ee9af6ec33f4dfaaaace5e4bab6c730fffc418d7
Msstt <1064263053@qq.com>
2025-02-11 22:32:23 +0800

    poll chat server

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Diagnostics;
+using System.Globalization;
 using System.Linq.Expressions;
 using System.Net;
 using System.Net.Sockets;
@@ -15,55 +17,51 @@ class MainClass {
     socket.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8888));
     socket.Listen(0);
     Console.WriteLine("Socket listen success!");
-    socket.BeginAccept(OnAccept, socket);
     while (true) {
-    }
-  }
-
-  private static void OnAccept(IAsyncResult ar) {
-    try {
-      Socket socket = (Socket)ar.AsyncState;
-      Socket client = socket.EndAccept(ar);
-      clients.Add(client, new ClientState {
-        socket = client
-      });
-      client.BeginReceive(clients[client].buffer, 0, 1024, 0, OnReceive, client);
-      socket.BeginAccept(OnAccept, socket);
-      Console.WriteLine("Accept client success!");
-    } catch (SocketException ex) {
-      Console.WriteLine("Accept client failed: " + ex);
-    }
-  }
-
-  private static void OnReceive(IAsyncResult ar) {
-    try {
-      Socket socket = (Socket)ar.AsyncState;
-      int bytesCount = socket.EndReceive(ar);
-      if (bytesCount == 0) {
-        socket.Close();
-        clients.Remove(socket);
-        Console.WriteLine("Client close.");
-        return;
+      if (socket.Poll(0, SelectMode.SelectRead)) {
+        ReadListenSocket(socket);
       }
-      string message = System.Text.Encoding.UTF8.GetString(clients[socket].buffer, 0, bytesCount);
-      Console.WriteLine("Receive message: " + message);
-      byte[] buffer = System.Text.Encoding.UTF8.GetBytes(socket.RemoteEndPoint.ToString() + ": " + message);
       foreach (Socket client in clients.Keys) {
-        client.BeginSend(buffer, 0, buffer.Length, 0, OnSend, client);
+        if (client.Poll(0, SelectMode.SelectRead)) {
+          if (!ReadClientSocket(client)) {
+            break;
+          }
+        }
       }
-      socket.BeginReceive(clients[socket].buffer, 0, 1024, 0, OnReceive, socket);
-    } catch (SocketException ex) {
-      Console.WriteLine("Socket receive failed: " + ex);
+      Thread.Sleep(1);
     }
   }
 
-  private static void OnSend(IAsyncResult ar) {
+  private static void ReadListenSocket(Socket socket) {
+    Socket client = socket.Accept();
+    clients.Add(client, new ClientState {
+      socket = client
+    });
+    Console.WriteLine("Accept client success!");
+  }
+
+  private static bool ReadClientSocket(Socket socket) {
+    int bytesCount = 0;
     try {
-      Socket socket = (Socket)ar.AsyncState;
-      int bytesCount = socket.EndSend(ar);
-      Console.WriteLine("Socket send " + bytesCount + " bytes success.");
+      bytesCount = socket.Receive(clients[socket].buffer);
     } catch (SocketException ex) {
-      Console.WriteLine("Socket send failed!" + ex);
+      socket.Close();
+      clients.Remove(socket);
+      Console.WriteLine("Socket receive failed: " + ex);
+      return false;
     }
+    if (bytesCount == 0) {
+      socket.Close();
+      clients.Remove(socket);
+      Console.WriteLine("Client close.");
+      return false;
+    }
+    string message = System.Text.Encoding.UTF8.GetString(clients[socket].buffer, 0, bytesCount);
+    Console.WriteLine("Receive message: " + message);
+    byte[] buffer = System.Text.Encoding.UTF8.GetBytes(socket.RemoteEndPoint.ToString() + ": " + message);
+    foreach (Socket client in clients.Keys) {
+      client.Send(buffer);
+    }
+    return true;
   }
 }
